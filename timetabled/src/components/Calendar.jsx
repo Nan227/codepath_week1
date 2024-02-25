f2c7d8d8cd18dc6cd09bc761d827f48ff6ccff50
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-02-24 19:48:03 -0500

    Added Event component

--- timetabled/src/components/Calendar.jsx
+++ timetabled/src/components/Calendar.jsx
@@ -1,31 +1,126 @@
 import React from "react";
+import Event from "./Event";
 
 const Calendar = () => {
     return (
-      <><div className ="Calendar">"Testing the calendar component"</div>
+      <div><div className ="Calendar">Weekly Calendar for Codpath students</div>
       <table>
-        <tr>
-          <th>Sunday</th>
-          <th>Monday</th>
-          <th>Tuesday</th>
-          <th>Wednesday</th>
-          <th>Thursday</th>
-          <th>Friday</th>
-          <th>Saturday</th>
-        </tr>
-        <tbody>
-          <td className = "time"> Insert Time </td>
-          <td>8:00 am</td>
-          <td>9:00 am</td> 
-          <td>10:00 am</td> 
-          <td>11:00 am</td> 
-          <td>01:00 pm</td> 
-          <td>02:00 pm</td> 
-          <td>03:00 pm</td>
-          <td>04:00 pm</td> 
-          <td>05:00 pm</td>
-        </tbody> 
-      </table></>
+  <thead>
+    <tr>
+      <th></th> {/* Empty header for the time column */}
+      <th>Sunday</th>
+      <th>Monday</th>
+      <th>Tuesday</th>
+      <th>Wednesday</th>
+      <th>Thursday</th>
+      <th>Friday</th>
+      <th>Saturday</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td className="time">8:00 am</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">9:00 am</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">10:00 am</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">11:00 am</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">12:00 pm</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">1:00 pm</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">2:00 pm</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">3:00 pm</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">4:00 pm</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+    <tr>
+      <td className="time">5:00 pm</td>
+      <td></td> {/* Empty cell for Sunday */}
+      <td></td> {/* Empty cell for Monday */}
+      <td></td> {/* Empty cell for Tuesday */}
+      <td></td> {/* Empty cell for Wednesday */}
+      <td></td> {/* Empty cell for Thursday */}
+      <td></td> {/* Empty cell for Friday */}
+      <td></td> {/* Empty cell for Saturday */}
+    </tr>
+  </tbody>
+</table>
+</div>
     )
 }
 export default Calendar;
---
+++ timetabled/src/components/Event.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Event = (props) => {
+    return (
+    
+            <><td className="Event">Event</td><h5>Test Event Name</h5></>
+      
+    );
+}
+export default Event;
